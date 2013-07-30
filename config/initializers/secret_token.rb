cedd7d9aa1b55faddd6a94841e30dfd8ce409764
Neeraj <neeraj.singh@60degree.com>
2013-07-30 16:35:21 +0530

    Embryo commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KnowMyBand::Application.config.secret_key_base = 'e82db1a6cda9e754cbf7a55462198d23819bafee8a9ef2985526074cd9bf50bf226c1792d9ba5b13eb986ed4a0949e368a534ebb349550a7f969daf184c09339'
